aa192d92f1f9b9c9758b15201ef0a6277e7f6b62
Felipe Garbi de Souza <feucontato7@gmail.com>
2026-08-11 21:57:43 -0300

    Meu Primeiro Código PHP

---
+++ mescla.php
@@ -0,0 +1,19 @@
+<?php 
+
+$mensagem = "O site está fora do ar";
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro Html e PHP</title>
+</head>
+<body>
+    <p><?php echo $mensagem ?></p>
+</body>
+</html>
